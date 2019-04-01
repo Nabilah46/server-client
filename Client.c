e62b150bb3d35688d896e74ed2094448ad9e9fb5
Nabilah46 <48412716+Nabilah46@users.noreply.github.com>
2019-04-01 23:02:10 +0800

    Update Client.c

--- Client.c
+++ Client.c
@@ -26,7 +26,7 @@ int main(int argc, char const *argv[])
     serv_addr.sin_port = htons(PORT); 
        
     // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "192.168.75.130", &serv_addr.sin_addr)<=0)  
+    if(inet_pton(AF_INET, "192.168.75.129", &serv_addr.sin_addr)<=0)  
     { 
         printf("\nInvalid address/ Address not supported \n"); 
         return -1; 
